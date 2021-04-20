dc8db7529c8a50ea0cbdeeb5a3943818f51716db
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2021-04-21 01:34:44 +0200

    Always create the mcu config file.
    This avoids klipper throwing an error when trying to include the file.

--- scripts/auto-detect-mcu.sh
+++ scripts/auto-detect-mcu.sh
@@ -3,6 +3,9 @@ CONFIG_PATH="/home/pi/klipper_config/v-core-3/mcu-detection.cfg"
 
 id="$(ls /dev/serial/by-id/usb-Klipper-* | head -n1)"
 
+# Make sure the file exists so there's no include error in klipper.
+touch "$CONFIG_PATH"
+
 if [[ -z "$id" ]]; then
     echo "$(date +"%Y-%m-%d %T"): MCU Detection Error: Cannot detect Klipper device" >> /tmp/klippy.log
     exit 1
