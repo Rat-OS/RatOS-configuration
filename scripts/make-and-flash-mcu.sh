d9dbf32d53a1f184cf5dab99dfd9d7ee2691e3a8
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2021-06-29 00:33:57 +0200

    Minor cleanup

--- scripts/make-and-flash-mcu.sh
+++ scripts/make-and-flash-mcu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Should be run as root.
-service klipper stop
+sudo service klipper stop
 cd /home/pi/klipper
 make clean
 make
 ./scripts/flash-sdcard.sh /dev/btt-skr-pro-12 btt-skr-pro-v1.2
-service klipper start
+sudo service klipper start
