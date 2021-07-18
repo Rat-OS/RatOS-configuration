9cddace481c7a3f8b788e963391eba4801c134bd
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2021-07-18 03:58:36 +0200

    Fix skr pro flashing script

---
+++ boards/btt-skr-pro-12/make-and-flash-mcu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp -f /home/pi/klipper_config/config/boards/btt-octopus-11/firmware.config /home/pi/klipper/.config
+cd /home/pi/klipper
+make olddefconfig
+make clean
+make
+sudo service klipper stop
+./scripts/flash-sdcard.sh /dev/klipper btt-skr-pro-v1.2
+if [$? -e 0]; then
+    echo "Flashing succesful!"
+else
+    echo "Flashing failed :("
+    sudo service klipper start
+    exit 1
+fi
+sudo service klipper start
