33a215b35aeed111c5c2c7a4de0dc4817bdf186a
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2021-10-16 02:43:16 +0200

    Boards: add BTT SKR 2

---
+++ boards/btt-skr-2-429/make-and-flash-mcu.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# NOTE: This script ONLY WORKS if the board has already been flashed with
+# klipper via SD card PRIOR to installing the boot0 jumper.
+# The first time the board is booted after the jumper has been installed
+# the board needs to be flashed via the dfu vendor:device id. After that
+# it can be flashed via the /dev/btt-skr-2-429 path, but it then fails
+# to exit dfu mode, and then one needs to flash it yet again with the
+# vendor:device id. Then it correctly exits DFU mode. Except those times
+# where it doesn't, for that we have a 3rd pass...
+
+
+MCU=/dev/btt-skr-2-429
+VENDORDEVICEID=0483:df11
+cp -f /home/pi/klipper_config/config/boards/btt-skr-2-429/firmware.config /home/pi/klipper/.config
+cd /home/pi/klipper
+make olddefconfig
+make clean
+make
+sudo service klipper stop
+if [ -e $MCU ]; then
+    echo "Flashing SKR 2 via path"
+    make flash FLASH_DEVICE=$MCU
+else
+    echo "Flashing SKR 2 via vendor and device ids - 1st pass"
+    make flash FLASH_DEVICE=$VENDORDEVICEID
+fi
+sleep 5
+if [ -e $MCU ]; then
+    echo "Flashing Successful!"
+else
+    echo "Flashing SKR 2 via vendor and device ids - 2nd pass"
+    make flash FLASH_DEVICE=$VENDORDEVICEID
+
+    sleep 5
+    if [ -e $MCU ]; then
+        echo "Flashing Successful!"
+    else
+        echo "Flashing SKR 2 via vendor and device ids - 3rd pass"
+        make flash FLASH_DEVICE=$VENDORDEVICEID
+        if [ $? -e 0 ]; then
+            echo "Flashing successful!"
+        else
+            echo "Flashing failed :("
+            sudo service klipper start
+            exit 1
+        fi
+    fi
+fi
+sudo service klipper start
