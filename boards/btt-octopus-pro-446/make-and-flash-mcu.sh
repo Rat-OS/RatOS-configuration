69009a379bedfd35b7841b64d1b9000bb1bc6ac0
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2021-10-20 03:04:53 +0200

    Boards: add octopus pro 429 and 446

---
+++ boards/btt-octopus-pro-446/make-and-flash-mcu.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# NOTE: This script ONLY WORKS if there's no SD card in the board AND the 
+# board has already been flashed with klipper via SD card.
+# NEXT SECTION IS ONLY RELEVANT IF USING THE boot0 JUMPER (NOT RECOMMENDED)
+# The first time the board is booted after the jumper has been installed
+# the board needs to be flashed via the dfu vendor:device id. After that
+# it can be flashed via the /dev/btt-octopus-pro-446 path, but it then fails
+# to exit dfu mode, and then one needs to flash it yet again with the
+# vendor:device id. Then it correctly exits DFU mode. Except those times
+# where it doesn't, for that we have a 3rd pass...
+
+
+MCU=/dev/btt-octopus-pro-446
+VENDORDEVICEID=0483:df11
+cp -f /home/pi/klipper_config/config/boards/btt-octopus-pro-446/firmware.config /home/pi/klipper/.config
+cd /home/pi/klipper
+make olddefconfig
+make clean
+make
+sudo service klipper stop
+if [ -e $MCU ]; then
+    echo "Flashing Octopus via path"
+    make flash FLASH_DEVICE=$MCU
+else
+    echo "Flashing Octopus via vendor and device ids - 1st pass"
+    make flash FLASH_DEVICE=$VENDORDEVICEID
+fi
+sleep 5
+if [ -e $MCU ]; then
+    echo "Flashing Successful!"
+else
+    echo "Flashing Octopus via vendor and device ids - 2nd pass"
+    make flash FLASH_DEVICE=$VENDORDEVICEID
+
+    sleep 5
+    if [ -e $MCU ]; then
+        echo "Flashing Successful!"
+    else
+        echo "Flashing Octopus via vendor and device ids - 3rd pass"
+        make flash FLASH_DEVICE=$VENDORDEVICEID
+        if [ $? -e 0 ]; then
+            echo "Flashing successful!"
+        else
+            echo "Flashing failed :("
+            sudo service klipper start
+            exit 1
+        fi
+    fi
+fi
+sudo service klipper start
