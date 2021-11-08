956b8de6da93ab52211980b5254b5df0c2d19111
david-mason-1992 <david@additiveautomations.com>
2021-11-08 21:36:42 +0000

    Add mellow super infinity compile script

--- scripts/compile-binaries.sh
+++ scripts/compile-binaries.sh
@@ -53,11 +53,11 @@ compile_skr_2_429() {
 }
 
 compile_mellow_super_infinity() {
-    cp -f /home/pi/klipper_config/config/boards/btt-skr-2-429/firmware.config /home/pi/klipper/.config
+    cp -f /home/pi/klipper_config/config/boards/mellow_super_infinity/firmware.config /home/pi/klipper/.config
     make olddefconfig
     make clean
     make
-    cp /home/pi/klipper/out/klipper.bin /home/pi/klipper_config/firmware_binaries/firmware-btt-skr-2-429.bin
+    cp /home/pi/klipper/out/klipper.bin /home/pi/klipper_config/firmware_binaries/mellow_super_infinity.bin
 }
 
 # Force script to exit if an error occurs
@@ -79,5 +79,6 @@ compile_btt_octopus_11
 compile_fysetc_spider
 compile_skr_pro_12
 compile_skr_2_429
+compile_mellow_super_infinity
 
 popd
