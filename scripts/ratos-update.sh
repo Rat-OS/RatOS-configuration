110254b5c88577401124d3a77149432cd88d4e80
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2021-11-14 15:14:57 +0100

    Scripts: restart klipper on RatOS update

--- scripts/ratos-update.sh
+++ scripts/ratos-update.sh
@@ -11,9 +11,14 @@ ensure_ownership() {
 
 update_symlinks()
 {
-    echo "Updating RatOS device symlinks.."
-    rm /etc/udev/rules.d/98-*.rules
-    ln -s /home/pi/klipper_config/config/boards/*/*.rules /etc/udev/rules.d/
+  echo "Updating RatOS device symlinks.."
+  rm /etc/udev/rules.d/98-*.rules
+  ln -s /home/pi/klipper_config/config/boards/*/*.rules /etc/udev/rules.d/
+}
+
+restart_klipper()
+{
+  service klipper restart
 }
 
 # Force script to exit if an error occurs
@@ -22,3 +27,4 @@ set -e
 # Run update symlinks
 update_symlinks
 ensure_ownership
+restart_klipper
