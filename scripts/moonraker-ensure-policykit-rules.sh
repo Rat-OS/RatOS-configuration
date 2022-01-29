9753fcb43df648a3449ade83c808d1eefe3af817
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-01-29 12:58:24 +0100

    Scripts: avoid restarting moonraker in githook
    The policykit script restarts moonraker at the end, so if this
    script is run as a moonraker post-merge git hook it will
    interrupt the moonraker install process. The policykit script
    now copies the script to tmp and comments out the systemctl
    command and then returns a value if moonraker needs to be restarted
    which is then used in the ratos-update script to restart moonraker
    instead. This way moonraker updates will still work smoothly, in case
    the user updated RatOS before updating Moonraker.

--- scripts/moonraker-ensure-policykit-rules.sh
+++ scripts/moonraker-ensure-policykit-rules.sh
@@ -6,32 +6,36 @@ POLKIT_USR_DIR="/usr/share/polkit-1/rules.d"
 MOONRAKER_UNIT="/etc/systemd/system/moonraker.service"
 
 ensure_moonraker_policiykit_rules() {
-	if [[ -e ${POLKIT_USR_DIR}/moonraker.rules ]]
-	then
-		echo -e "\n\n###### Moonraker policy exists, skipping policykit script."
-		return
-	fi
-	if [[ -e ${POLKIT_DIR}/moonraker.rules ]]
-	then
-		echo -e "\n\n###### Moonraker policy exists, skipping policykit script."
-		return
-	fi
-	if [[ -e ${POLKIT_LEGACY_DIR}/10-moonraker.pkla ]]
-	then
-		echo -e "\n\n###### Moonraker legacy policy exists, skipping policykit script."
-		return
-	fi
+	# if [[ -e ${POLKIT_USR_DIR}/moonraker.rules ]]
+	# then
+	# 	echo -e "\n\n###### Moonraker policy exists, skipping policykit script."
+	# 	return
+	# fi
+	# if [[ -e ${POLKIT_DIR}/moonraker.rules ]]
+	# then
+	# 	echo -e "\n\n###### Moonraker policy exists, skipping policykit script."
+	# 	return
+	# fi
+	# if [[ -e ${POLKIT_LEGACY_DIR}/10-moonraker.pkla ]]
+	# then
+	# 	echo -e "\n\n###### Moonraker legacy policy exists, skipping policykit script."
+	# 	return
+	# fi
 	if [[ -e /home/pi/moonraker/scripts/set-policykit-rules.sh ]]
 	then
+		cp /home/pi/moonraker/scripts/set-policykit-rules.sh /tmp/set-policykit-rules.sh
+		# if moonraker restarts the update process will be terminated, leaving a broken moonraker install.
+		sed -i 's/sudo systemctl restart moonraker/#sudo systemctl restart moonraker/' /tmp/set-policykit-rules.sh
 		echo -e "\n\n###### Moonraker policy not found, running moonraker policykit script..."
 		if [ "$EUID" -eq 0 ]; then
 			# This feels wrong, but...
 			OLDUSER=$USER
 			USER=pi
-			/home/pi/moonraker/scripts/set-policykit-rules.sh
+			/tmp/set-policykit-rules.sh
 			USER=$OLDUSER
 		else
-			/home/pi/moonraker/scripts/set-policykit-rules.sh
+			/tmp/set-policykit-rules.sh
 		fi
+		return 1
 	fi
 }
