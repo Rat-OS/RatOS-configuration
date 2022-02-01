674159b95658510d332c7e959e70f77fa92faa9d
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-02-01 11:19:31 +0100

    Reenable the moonraker-update polickit fix

--- scripts/moonraker-update.sh
+++ scripts/moonraker-update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# source /home/pi/klipper_config/config/scripts/moonraker-ensure-policykit-rules.sh
-# ensure_moonraker_policiykit_rules
+source /home/pi/klipper_config/config/scripts/moonraker-ensure-policykit-rules.sh
+ensure_moonraker_policiykit_rules
