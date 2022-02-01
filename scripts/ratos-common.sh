d7ba8d73e6ff84c45b9f98d00bfd289a76268a10
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-02-01 11:55:38 +0100

    Scripts: do command whitelisting as root
    This fixes RatOS install during image compilation

--- scripts/ratos-common.sh
+++ scripts/ratos-common.sh
@@ -27,29 +27,29 @@ ensure_sudo_command_whitelisting()
 	# Whitelist RatOS git hook scripts
 	if [[ -e /etc/sudoers.d/030-ratos-githooks ]]
 	then
-		rm /etc/sudoers.d/030-ratos-githooks
+		sudo rm /etc/sudoers.d/030-ratos-githooks
 	fi
-	touch /tmp/030-ratos-githooks
-	cat << '#EOF' > /tmp/030-ratos-githooks
+	sudo touch /tmp/030-ratos-githooks
+	sudo cat << '#EOF' > /tmp/030-ratos-githooks
 pi  ALL=(ALL) NOPASSWD: /home/pi/klipper_config/config/scripts/ratos-update.sh
 pi  ALL=(ALL) NOPASSWD: /home/pi/klipper_config/config/scripts/klipper-mcu-update.sh
 pi  ALL=(ALL) NOPASSWD: /home/pi/klipper_config/config/scripts/moonraker-update.sh
 #EOF
 
-	chown root:root /tmp/030-ratos-githooks
-	chmod 440 /tmp/030-ratos-githooks
-	cp --preserve=mode /tmp/030-ratos-githooks /etc/sudoers.d/030-ratos-githooks
+	sudo chown root:root /tmp/030-ratos-githooks
+	sudo chmod 440 /tmp/030-ratos-githooks
+	sudo cp --preserve=mode /tmp/030-ratos-githooks /etc/sudoers.d/030-ratos-githooks
 
 	# Whitelist change hostname script
 	if [[ ! -e /etc/sudoers.d/031-ratos-change-hostname ]]
 	then
-		touch /tmp/031-ratos-change-hostname
-		cat << '#EOF' > /tmp/031-ratos-change-hostname
+		sudo touch /tmp/031-ratos-change-hostname
+		sudo cat << '#EOF' > /tmp/031-ratos-change-hostname
 pi  ALL=(ALL) NOPASSWD: /home/pi/klipper_config/config/scripts/change-hostname-as-root.sh
 #EOF
 
-		chown root:root /tmp/031-ratos-change-hostname
-		chmod 440 /tmp/031-ratos-change-hostname
-		cp --preserve=mode /tmp/031-ratos-change-hostname /etc/sudoers.d/031-ratos-change-hostname
+		sudo chown root:root /tmp/031-ratos-change-hostname
+		sudo chmod 440 /tmp/031-ratos-change-hostname
+		sudo cp --preserve=mode /tmp/031-ratos-change-hostname /etc/sudoers.d/031-ratos-change-hostname
 	fi
 }
