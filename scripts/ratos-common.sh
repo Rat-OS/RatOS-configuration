cac81d3709c1a271f015dac3bf152c8628e3d585
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-02-13 02:32:40 +0100

    Scripts: sudo switching fix for ratos-common

--- scripts/ratos-common.sh
+++ scripts/ratos-common.sh
@@ -23,33 +23,38 @@ install_hooks()
 
 ensure_sudo_command_whitelisting()
 {
+	sudo="sudo"
+	if [ $1 -eq "root" ]
+	then
+		sudo=""
+	fi
     report_status "Updating whitelisted commands"
 	# Whitelist RatOS git hook scripts
 	if [[ -e /etc/sudoers.d/030-ratos-githooks ]]
 	then
-		sudo rm /etc/sudoers.d/030-ratos-githooks
+		$sudo rm /etc/sudoers.d/030-ratos-githooks
 	fi
-	sudo touch /tmp/030-ratos-githooks
-	sudo cat << '#EOF' > /tmp/030-ratos-githooks
+	$sudo touch /tmp/030-ratos-githooks
+	$sudo cat << '#EOF' > /tmp/030-ratos-githooks
 pi  ALL=(ALL) NOPASSWD: /home/pi/klipper_config/config/scripts/ratos-update.sh
 pi  ALL=(ALL) NOPASSWD: /home/pi/klipper_config/config/scripts/klipper-mcu-update.sh
 pi  ALL=(ALL) NOPASSWD: /home/pi/klipper_config/config/scripts/moonraker-update.sh
 #EOF
 
-	sudo chown root:root /tmp/030-ratos-githooks
-	sudo chmod 440 /tmp/030-ratos-githooks
-	sudo cp --preserve=mode /tmp/030-ratos-githooks /etc/sudoers.d/030-ratos-githooks
+	$sudo chown root:root /tmp/030-ratos-githooks
+	$sudo chmod 440 /tmp/030-ratos-githooks
+	$sudo cp --preserve=mode /tmp/030-ratos-githooks /etc/sudoers.d/030-ratos-githooks
 
 	# Whitelist change hostname script
 	if [[ ! -e /etc/sudoers.d/031-ratos-change-hostname ]]
 	then
-		sudo touch /tmp/031-ratos-change-hostname
-		sudo cat << '#EOF' > /tmp/031-ratos-change-hostname
+		$sudo touch /tmp/031-ratos-change-hostname
+		$sudo cat << '#EOF' > /tmp/031-ratos-change-hostname
 pi  ALL=(ALL) NOPASSWD: /home/pi/klipper_config/config/scripts/change-hostname-as-root.sh
 #EOF
 
-		sudo chown root:root /tmp/031-ratos-change-hostname
-		sudo chmod 440 /tmp/031-ratos-change-hostname
-		sudo cp --preserve=mode /tmp/031-ratos-change-hostname /etc/sudoers.d/031-ratos-change-hostname
+		$sudo chown root:root /tmp/031-ratos-change-hostname
+		$sudo chmod 440 /tmp/031-ratos-change-hostname
+		$sudo cp --preserve=mode /tmp/031-ratos-change-hostname /etc/sudoers.d/031-ratos-change-hostname
 	fi
 }
