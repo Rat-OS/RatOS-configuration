8ebecb451a17f81e3fb73c905b0d4e86a04cdbd1
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-04-22 02:58:16 +0200

    Toolboards: add mellow fly-sht-42

--- scripts/compile-binaries.sh
+++ scripts/compile-binaries.sh
@@ -54,6 +54,15 @@ compile_skr_2_429() {
     cp /home/pi/klipper/out/klipper.bin /home/pi/klipper_config/firmware_binaries/firmware-btt-skr-2-429.bin
 }
 
+compile_mellow_fly_sht_42() {
+    echo "Compiling firmware for Mellow FLY-SHT42"
+    cp -f /home/pi/klipper_config/config/boards/mellow-fly-sht-42/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/klipper_config/firmware_binaries/firmware-mellow-fly-sht-42.bin
+}
+
 # Force script to exit if an error occurs
 set -e
 
@@ -73,6 +82,7 @@ compile_btt_octopus_11
 compile_fysetc_spider
 compile_skr_pro_12
 compile_skr_2_429
+compile_mellow_fly_sht_42
 chown pi:pi /home/pi/klipper_config/firmware_binaries/*.bin
 
 popd
