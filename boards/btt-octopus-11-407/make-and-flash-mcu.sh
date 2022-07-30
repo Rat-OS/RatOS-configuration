366738e43b5b01c289ea99dc1d53555ae7799e0f
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-07-30 13:26:23 +0200

    Boards: update octopus f407 board definition

--- boards/btt-octopus-11-407/make-and-flash-mcu.sh
+++ boards/btt-octopus-11-407/make-and-flash-mcu.sh
@@ -4,7 +4,7 @@
 # NEXT SECTION IS ONLY RELEVANT IF USING THE boot0 JUMPER (NOT RECOMMENDED)
 # The first time the board is booted after the jumper has been installed
 # the board needs to be flashed via the dfu vendor:device id. After that
-# it can be flashed via the /dev/btt-octopus-11-407 path, but it then fails
+# it can be flashed via the /dev/btt-octopus-11 path, but it then fails
 # to exit dfu mode, and then one needs to flash it yet again with the
 # vendor:device id. Then it correctly exits DFU mode. Except those times
 # where it doesn't, for that we have a 3rd pass...
@@ -15,54 +15,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-MCU=/dev/btt-octopus-11-407
-VENDORDEVICEID=0483:df11
-cp -f /home/pi/klipper_config/config/boards/btt-octopus-11-407/firmware.config /home/pi/klipper/.config
-pushd /home/pi/klipper
-make olddefconfig
-make clean
-make
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ ! -d "/home/pi/klipper_config/firmware_binaries" ]
-then
-    mkdir /home/pi/klipper_config/firmware_binaries
-    chown pi:pi /home/pi/klipper_config/firmware_binaries
-fi
-cp -f /home/pi/klipper/out/klipper.bin /home/pi/klipper_config/firmware_binaries/firmware-btt-octopus-11-407.bin
-chown pi:pi /home/pi/klipper_config/firmware_binaries/firmware-btt-octopus-11-407.bin
-
-service klipper stop
-if [ -h $MCU ]; then
-    echo "Flashing Octopus via path"
-    make flash FLASH_DEVICE=$MCU
-else
-    echo "Flashing Octopus via vendor and device ids - 1st pass"
-    make flash FLASH_DEVICE=$VENDORDEVICEID
-fi
-sleep 5
-if [ -h $MCU ]; then
-    echo "Flashing Successful!"
-else
-    echo "Flashing Octopus via vendor and device ids - 2nd pass"
-    make flash FLASH_DEVICE=$VENDORDEVICEID
-
-    sleep 5
-    if [ -h $MCU ]; then
-        echo "Flashing Successful!"
-    else
-        echo "Flashing Octopus via vendor and device ids - 3rd pass"
-        make flash FLASH_DEVICE=$VENDORDEVICEID
-        if [ $? -e 0 ]; then
-            echo "Flashing successful!"
-        else
-            echo "Flashing failed :("
-            service klipper start
-            popd
-            chown pi:pi -R /home/pi/klipper
-            exit 1
-        fi
-    fi
-fi
-chown pi:pi -R /home/pi/klipper
-service klipper start
-popd
+$SCRIPT_DIR/compile.sh
+$SCRIPT_DIR/flash.sh
