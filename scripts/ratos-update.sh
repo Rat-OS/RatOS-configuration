82911b5e4ef89a1a18b248b5037f457ea1c59ee3
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-10-16 01:22:48 +0200

    Scripts: move config validation to moonraker update
    Move the moonraker config validation to the moonraker post-merge
    hook. This way the user won't see a dirt RatOS install at all
    and things are less likely to go wrong.

--- scripts/ratos-update.sh
+++ scripts/ratos-update.sh
@@ -45,20 +45,6 @@ fix_klipperscreen_permissions()
 {
   chown -R pi /home/pi/KlipperScreen
 }
-
-validate_moonraker_config()
-{
-  echo "Ensuring valid moonraker config.."
-  install_version=$(/home/pi/moonraker-env/bin/python -mlmdb -e /home/pi/.moonraker_database/ -d moonraker get validate_install)
-  cat /home/pi/klipper_config/moonraker.conf | grep "[include config/moonraker.conf]" > /dev/null
-  has_include=$?
-  if [ $has_include -eq 0 ] && [ "$install_version" = "'validate_install': missing" ]; then
-    # Temporarily replace with old config
-    echo "Installing old moonraker config.."
-    cp /home/pi/klipper_config/config/old-moonraker.conf /home/pi/klipper_config/config/moonraker.conf
-  fi
-}
-
 # Run update symlinks
 update_symlinks
 ensure_ownership
@@ -68,5 +54,4 @@ ensure_moonraker_policiykit_rules
 [ $? -eq 1 ] && echo "Policykit rules have changed. You will have to manually restart moonraker. Power cycling the raspberry pi will also do the trick."
 fix_klipperscreen_forcepush
 fix_klipperscreen_permissions
-validate_moonraker_config
 restart_klipper
