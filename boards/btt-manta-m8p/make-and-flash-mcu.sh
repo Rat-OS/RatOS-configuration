a6e1de61071ceac93b702e0b09d33e7b5ec28020
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-10-24 00:13:57 +0200

    Boards: minor correction to manta8p
    Scripts weren't executable.
    Some copy paste errors.

--- boards/btt-manta-m8p/make-and-flash-mcu.sh
+++ boards/btt-manta-m8p/make-and-flash-mcu.sh
@@ -14,58 +14,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-MCU=/dev/btt-manta-m8p
-VENDORDEVICEID=0483:df11
-cp -f /home/pi/klipper_config/config/boards/btt-manta-m8p/firmware.config /home/pi/klipper/.config
-pushd /home/pi/klipper
-make olddefconfig
-make clean
-make
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$SCRIPT_DIR/compile.sh
+$SCRIPT_DIR/flash.sh
 
-if [ ! -d "/home/pi/klipper_config/firmware_binaries" ]
-then
-    mkdir /home/pi/klipper_config/firmware_binaries
-    chown pi:pi /home/pi/klipper_config/firmware_binaries
-fi
-cp -f /home/pi/klipper/out/klipper.bin /home/pi/klipper_config/firmware_binaries/firmware-btt-manta-m8p.bin
-chown pi:pi /home/pi/klipper_config/firmware_binaries/firmware-btt-manta-m8p.bin
-
-# Reset ownership
-chown pi:pi -R /home/pi/klipper
-
-# echo "Unfortunately, unlike the Manta M8P cannot currently be flashed via DFU. The file firmware-btt-manta-m8p.bin has been compiled and is available in the firmware_binaries folder in Mainsail under the Machine tab. Use this to flash via SD Card." 
-# echo "NOTE: Remember to rename the file to firmware.bin on the SD Card!"
-
-service klipper stop
-if [ -h $MCU ]; then
-    echo "Flashing Manta M8P via path"
-    make flash FLASH_DEVICE=$MCU
-else
-    echo "Flashing Manta M8P via vendor and device ids - 1st pass"
-    make flash FLASH_DEVICE=$VENDORDEVICEID
-fi
-sleep 5
-if [ -h $MCU ]; then
-    echo "Flashing Successful!"
-else
-    echo "Flashing Manta M8P via vendor and device ids - 2nd pass"
-    make flash FLASH_DEVICE=$VENDORDEVICEID
-
-    sleep 5
-    if [ -h $MCU ]; then
-        echo "Flashing Successful!"
-    else
-        echo "Flashing Manta M8P via vendor and device ids - 3rd pass"
-        make flash FLASH_DEVICE=$VENDORDEVICEID
-        if [ $? -e 0 ]; then
-            echo "Flashing successful!"
-        else
-            echo "Flashing failed :("
-            service klipper start
-            popd
-            exit 1
-        fi
-    fi
-fi
-service klipper start
-popd
