bd2c8d9b62c6f6dfd2b08b074a6aa61b832a46b6
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-11-17 00:08:25 +0100

    Boards: switch to new flash method

--- boards/btt-skr-3-ez/flash.sh
+++ boards/btt-skr-3-ez/flash.sh
@@ -5,22 +5,4 @@ if [ "$EUID" -ne 0 ]
   then echo "ERROR: Please run as root"
   exit
 fi
-pushd /home/pi/klipper
-service klipper stop
-if [ -h $MCU ]; then
-    echo "Flashing BTT SKR 3 EZ via path"
-    make flash FLASH_DEVICE=$MCU
-fi
-sleep 5
-if [ -h $MCU ]; then
-    echo "Flashing Successful!"
-else
-	echo "Flashing failed :("
-	service klipper start
-	popd
-	chown pi:pi -R /home/pi/klipper
-	exit 1
-fi
-chown pi:pi -R /home/pi/klipper
-service klipper start
-popd
+../../scripts/flash-path.sh $MCU
