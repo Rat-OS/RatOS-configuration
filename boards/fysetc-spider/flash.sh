4973a4ba3641ab201b2bc0ed9617505058f2bca0
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-11-20 04:32:11 +0100

    Boards: fix path to flash script

--- boards/fysetc-spider/flash.sh
+++ boards/fysetc-spider/flash.sh
@@ -5,4 +5,6 @@ if [ "$EUID" -ne 0 ]
   then echo "ERROR: Please run as root"
   exit
 fi
-../../scripts/flash-path.sh $MCU
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FLASH_SCRIPT=$(realpath "$SCRIPT_DIR/../../scripts/flash-path.sh")
+$FLASH_SCRIPT $MCU
