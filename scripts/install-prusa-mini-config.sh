89265525edd0f641386db0ffded2c721a95556c1
John Scancella <blacksmithforlife@gmail.com>
2023-02-14 19:20:36 -0500

    feat: rename raspberry pi mcu when needed

--- scripts/install-prusa-mini-config.sh
+++ scripts/install-prusa-mini-config.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-tail -n +2 /home/pi/klipper_config/config/templates/prusa-mini-printer.template.cfg > /home/pi/klipper_config/printer.cfg
+tail -n +2 /home/pi/printer_data/config/RatOS/template/prusa-mini-printer.template.cfg > /home/pi/printer_data/config/RatOS/printer.cfg
+
+#on rasbian get the board name
+function get_sbc  {
+    cat /etc/board-release | grep BOARD_NAME | cut -d '=' -f2 | cut -d '"' -f2
+}
+
+#change the mainsail interface to use the correct board name
+if [ -e /etc/board-release ]
+then
+  git -C /home/pi/printer_data/config/RatOS update-index --assume-unchanged /home/pi/printer_data/config/RatOS/printers/sbc.cfg
+  sed -i s"/raspberry_pi/$(get_sbc)/g" /home/pi/printer_data/config/RatOS/printers/sbc.cfg
+fi
--- scripts/install-v-core-pro-config.sh
+++ scripts/install-v-core-pro-config.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 tail -n +2 /home/pi/printer_data/config/RatOS/templates/v-core-pro-printer.template.cfg > /home/pi/printer_data/config/printer.cfg
+
+#on rasbian get the board name
+function get_sbc  {
+    cat /etc/board-release | grep BOARD_NAME | cut -d '=' -f2 | cut -d '"' -f2
+}
+
+#change the mainsail interface to use the correct board name
+if [ -e /etc/board-release ]
+then
+  git -C /home/pi/printer_data/config/RatOS update-index --assume-unchanged /home/pi/printer_data/config/RatOS/printers/sbc.cfg
+  sed -i s"/raspberry_pi/$(get_sbc)/g" /home/pi/printer_data/config/RatOS/printers/sbc.cfg
+fi
