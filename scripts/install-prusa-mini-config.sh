651be84c554f46050d80b001523c6947303b8847
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2023-02-16 19:30:41 +0100

    Printers: fix invalid paths for prusa mini

--- scripts/install-prusa-mini-config.sh
+++ scripts/install-prusa-mini-config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail -n +2 /home/pi/klipper_config/config/templates/prusa-mini-printer.template.cfg > /home/pi/klipper_config/printer.cfg
+tail -n +2 /home/pi/printer_data/config/RatOS/templates/prusa-mini-printer.template.cfg > /home/pi/printer_data/config/printer.cfg
