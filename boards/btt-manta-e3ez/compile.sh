a84a03ac686ac033f89775cdcc1bdf5d526d02f3
joaobarros <joao.barros@gmail.com>
2023-02-26 23:32:20 +0000

    Initial BTT Manta E3EZ support

---
+++ boards/btt-manta-e3ez/compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "ERROR: Please run as root"
+  exit
+fi
+#cp -f /home/pi/klipper_config/config/boards/btt-manta-e3ez/firmware.config /home/pi/klipper/.config
+cp -f /home/pi/klipper_config/btt-manta-e3ez/firmware.config /home/pi/klipper/.config
+pushd /home/pi/klipper
+make olddefconfig
+make clean
+make
+
+if [ ! -d "/home/pi/klipper_config/firmware_binaries" ]
+then
+    mkdir /home/pi/klipper_config/firmware_binaries
+    chown pi:pi /home/pi/klipper_config/firmware_binaries
+fi
+cp -f /home/pi/klipper/out/klipper.bin /home/pi/klipper_config/firmware_binaries/firmware-btt-manta-e3ez.bin
+chown pi:pi /home/pi/klipper_config/firmware_binaries/firmware-btt-manta-e3ez.bin
+popd
