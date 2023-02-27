f86bae169cd544f8508a6d957aaa83a4a0caf99d
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2023-02-28 00:08:55 +0100

    Scripts: fix service permissions on moonraker update

--- scripts/moonraker-update.sh
+++ scripts/moonraker-update.sh
@@ -18,4 +18,14 @@ validate_moonraker_config()
   fi
 }
 
+ensure_service_permission()
+{
+	report_status "Updating service permissions"
+	if ! cat /home/pi/printer_data/moonraker.asvc | grep "ratos-configurator" &>/dev/null; then
+		printf '\nratos-configurator' >> /home/pi/printer_data/moonraker.asvc
+		report_status "Configurator added to moonraker service permissions"
+	fi
+}
+
 validate_moonraker_config
+ensure_service_permission
