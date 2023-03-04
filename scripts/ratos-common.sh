472e24ad75a18cabec733329df88c1286f830244
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2023-03-04 02:46:21 +0100

    Scripts: write entire moonraker.asvc file if necessary

--- scripts/ratos-common.sh
+++ scripts/ratos-common.sh
@@ -79,8 +79,20 @@ install_hooks()
 ensure_service_permission()
 {
 	report_status "Updating service permissions"
-	if ! cat /home/pi/printer_data/moonraker.asvc | grep "ratos-configurator" &>/dev/null; then
-		printf '\nratos-configurator' >> /home/pi/printer_data/moonraker.asvc
+	if ! cat /home/pi/printer_data/moonraker.asvc | grep "klipper_mcu" &>/dev/null; then
+		cat << '#EOF' > /home/pi/printer_data/moonraker.asvc
+klipper_mcu
+webcamd
+MoonCord
+KlipperScreen
+moonraker-telegram-bot
+moonraker-obico
+sonar
+crowsnest
+octoeverywhere
+ratos-configurator
+#EOF
+
 		report_status "Configurator added to moonraker service permissions"
 	fi
 }
