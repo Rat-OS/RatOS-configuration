c98b10036bc43c0af52b321c90e908dd9a9a92fc
Pete H <pete@kawumtech.de>
2023-03-21 20:04:29 +0100

    fix install script paths

--- scripts/install-hypercube-evolution-config.sh
+++ scripts/install-hypercube-evolution-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tail -n +2 /home/pi/klipper_config/config/templates/hypercube-evolution-printer.template.cfg > /home/pi/klipper_config/printer.cfg
+tail -n +2 /home/pi/printer_data/config/RatOS/templates/hypercube-evolution-printer.template.cfg > /home/pi/printer_data/config/printer.cfg
 
