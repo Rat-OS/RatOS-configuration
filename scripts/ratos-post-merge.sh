056fe0be71984992c690e8aa0f6d765442984fbb
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2023-04-03 03:03:59 +0200

    Scripts: fix source references

--- scripts/ratos-post-merge.sh
+++ scripts/ratos-post-merge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# shellcheck source=./ratos-update.sh
+# shellcheck source=./scripts/ratos-update.sh
 sudo "$SCRIPT_DIR"/ratos-update.sh
