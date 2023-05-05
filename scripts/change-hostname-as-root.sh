33002f2091232c57473872d729bbb9ba8c47a7f7
plaintext <acorretti@gmail.com>
2023-05-06 01:25:17 +0200

    Renames RPi usage to generic host computer

--- scripts/change-hostname-as-root.sh
+++ scripts/change-hostname-as-root.sh
@@ -16,7 +16,7 @@ echo $1 > /etc/hostname
 sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$1/g" /etc/hosts
 if [ $? -eq 0 ] 
 then
-	echo "Hostname has been changed, please reboot your Raspberry Pi for the change to take effect"
+	echo "Hostname has been changed, please reboot your host computer for the change to take effect"
 else
 	echo "An error occured while attempting to change the hostname"
 	exit 1
