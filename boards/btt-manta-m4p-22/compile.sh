55f98cd7004be3575188d0ab346772849263f930
Mikkel Schmidt <msc@orbit.online>
2023-10-30 03:17:07 +0100

    Boards: fix bash linting

--- boards/btt-manta-m4p-22/compile.sh
+++ boards/btt-manta-m4p-22/compile.sh
@@ -4,7 +4,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 cp -f /home/pi/printer_data/config/RatOS/boards/btt-manta-m4p-22/firmware.config /home/pi/klipper/.config
-pushd /home/pi/klipper
+pushd /home/pi/klipper || exit
 make olddefconfig
 make clean
 make
@@ -16,4 +16,4 @@ then
 fi
 cp -f /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-manta-m4p-22.bin
 chown pi:pi /home/pi/printer_data/config/firmware_binaries/firmware-btt-manta-m4p-22.bin
-popd
+popd || exit
