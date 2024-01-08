74c15d6c81f3f46d6f86d55288c4820a05c1c31b
Mikkel Schmidt <msc@orbit.online>
2024-01-08 19:49:53 +0100

    Remove old printer installation macros

--- scripts/install-v-core-pro-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-tail -n +2 "$SCRIPT_DIR"/../templates/v-core-pro-printer.template.cfg > "$SCRIPT_DIR"/../../printer.cfg
--- scripts/install-voron-v01-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-tail -n +2 "$SCRIPT_DIR"/../templates/voron-v01-printer.template.cfg > "$SCRIPT_DIR"/../../printer.cfg
