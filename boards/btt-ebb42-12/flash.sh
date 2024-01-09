8e33216c0c8812822d35d1b6465820f53149e5a9
sergelhr <93795552+sergelhr@users.noreply.github.com>
2024-01-09 12:37:58 +0100

    Delete boards/btt-ebb42-12 directory

--- boards/btt-ebb42-12/flash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MCU=/dev/btt-ebb42-12
-if [ "$EUID" -ne 0 ]
-  then echo "ERROR: Please run as root"
-  exit
-fi
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-FLASH_SCRIPT=$(realpath "$SCRIPT_DIR/../../scripts/flash-path.sh")
-$FLASH_SCRIPT $MCU
