b8c26f7b58d3547ac8bdcd9b359599f078801e6d
Mikkel Schmidt <msc@orbit.online>
2024-01-14 19:50:26 +0100

    Scripts: mute find permission errors

--- scripts/generate-shaper-graph-x.sh
+++ scripts/generate-shaper-graph-x.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-NEWX=$(find /tmp -name "resonances_x_*.csv" -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+NEWX=$(find /tmp -name "resonances_x_*.csv" -printf '%T@ %p\n' 2> /dev/null | sort -n | tail -1 | cut -f2- -d" ")
 DATE=$(date +'%Y-%m-%d-%H%M%S')
 if [ ! -d "$SCRIPT_DIR/../../input_shaper" ]
 then
--- scripts/generate-shaper-graph-y.sh
+++ scripts/generate-shaper-graph-y.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-NEWY=$(find /tmp -name "resonances_y_*.csv" -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+NEWY=$(find /tmp -name "resonances_y_*.csv" -printf '%T@ %p\n' 2> /dev/null | sort -n | tail -1 | cut -f2- -d" ")
 DATE=$(date +'%Y-%m-%d-%H%M%S')
 if [ ! -d "$SCRIPT_DIR/../../input_shaper" ]
 then
