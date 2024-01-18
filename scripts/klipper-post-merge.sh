4b267cd4b2106b59c2b8255dd01a19723f5709d9
Mikkel Schmidt <msc@orbit.online>
2024-01-18 23:44:25 +0100

    Scripts: fix script dir when symlinked

--- scripts/klipper-post-merge.sh
+++ scripts/klipper-post-merge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
 # shellcheck source=./scripts/klipper-mcu-update.sh
 sudo "$SCRIPT_DIR"/klipper-mcu-update.sh
--- scripts/moonraker-post-merge.sh
+++ scripts/moonraker-post-merge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
 # shellcheck source=./scripts/moonraker-update.sh
 sudo "$SCRIPT_DIR"/moonraker-update.sh
