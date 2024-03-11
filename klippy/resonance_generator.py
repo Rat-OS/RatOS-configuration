96848b24116a00e9c48f84f9b9c61d1cdb7dc30c
Mikkel Schmidt <msc@orbit.online>
2024-03-11 04:08:06 +0100

    Klippy: resgen -> oscillate and generate_resonances

--- klippy/resonance_generator.py
+++ klippy/resonance_generator.py
@@ -13,16 +13,17 @@ class VibrationGenerator:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.gcode = self.printer.lookup_object('gcode')
-        self.accel_per_hz = config.getfloat('accel_per_hz', 75., above=0.)
+        tester_config = config.getsection('resonance_tester')
+        self.accel_per_hz = tester_config.getfloat('accel_per_hz', 75., above=0.)
         # Resonate for 1 second by default
-        self.time = config.getfloat('time', 1, minval=0.1)
+        self.time = 1
 
     def prepare_test(self, gcmd):
         self.freq = gcmd.get_float("FREQ", None, minval=1., maxval=300.)
         if self.freq is None:
             raise gcmd.error("FREQ parameter is required")
 
-        self.time = gcmd.get_float("TIME", self.time, minval=1., maxval=60.)
+        self.time = gcmd.get_float("TIME", 1, minval=1., maxval=60.)
 
     def run_test(self, axis, gcmd):
         toolhead = self.printer.lookup_object('toolhead')
@@ -79,14 +80,37 @@ class VibrationGenerator:
 class ResonanceGenerator:
     def __init__(self, config):
         self.printer = config.get_printer()
-        self.generator = VibrationGenerator(config)
+        self.oscillator = VibrationGenerator(config)
+        tester_config = config.getsection('resonance_tester')
+        self.generator = resonance_tester.VibrationPulseTest(tester_config)
 
         self.gcode = self.printer.lookup_object('gcode')
+        self.gcode.register_command("OSCILLATE",
+                                    self.cmd_OSCILLATE,
+                                    desc=self.cmd_OSCILLATE_help)
         self.gcode.register_command("GENERATE_RESONANCES",
                                     self.cmd_GENERATE_RESONANCES,
                                     desc=self.cmd_GENERATE_RESONANCES_help)
 
-    def _run_test(self, gcmd, axes):
+    def _oscillate(self, gcmd, axes):
+        toolhead: ToolHead = self.printer.lookup_object('toolhead')
+        if not isinstance(toolhead, ToolHead):
+            raise gcmd.error("Toolhead is not available")
+
+        self.oscillator.prepare_test(gcmd)
+
+        gcmd.respond_info(
+                "Generating oscillations at (%.3f, %.3f, %.3f)" % tuple(toolhead.get_position()[:3]))
+        for axis in axes:
+            toolhead.wait_moves()
+            toolhead.dwell(0.500)
+            if len(axes) > 1:
+                gcmd.respond_info("Testing axis %s" % axis.get_name())
+
+            # Generate moves
+            self.oscillator.run_test(axis, gcmd)
+
+    def _generate_resonances(self, gcmd, axes):
         toolhead: ToolHead = self.printer.lookup_object('toolhead')
         if not isinstance(toolhead, ToolHead):
             raise gcmd.error("Toolhead is not available")
@@ -107,12 +131,24 @@ class ResonanceGenerator:
     def _get_max_calibration_freq(self):
         return 1.5 * self.generator.get_max_freq()
 
-    cmd_GENERATE_RESONANCES_help = ("Generates resonances for a specifed axis and frequency.")
+    cmd_OSCILLATE_help = ("Oscillate AXIS at the given FREQ for TIME amount of seconds.")
+    def cmd_OSCILLATE(self, gcmd):
+        # Parse parameters
+        axis = resonance_tester._parse_axis(gcmd, gcmd.get("AXIS").lower())
+
+        self._oscillate(
+                gcmd, [axis])
+
+        gcmd.respond_info(
+                "Completed resonance generation for %s axis" % (axis.get_name()))
+
+
+    cmd_GENERATE_RESONANCES_help = ("Generate resonances for AXIS from FREQ_START to FREQ_END hz.")
     def cmd_GENERATE_RESONANCES(self, gcmd):
         # Parse parameters
         axis = resonance_tester._parse_axis(gcmd, gcmd.get("AXIS").lower())
 
-        self._run_test(
+        self._generate_resonances(
                 gcmd, [axis])
 
         gcmd.respond_info(
