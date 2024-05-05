2378346286286dff23e41f01c1cdf8e3d68ad5d6
HelgeKeck <helgekeck@hotmail.com>
2024-05-05 09:15:28 +0200

    client post processor readded nad updated to be compatible with rmmu

--- scripts/ratos_postprocessor.py
+++ scripts/ratos_postprocessor.py
@@ -1,82 +1,58 @@
-# RatOS IDEX and RMMU Gcode Post Processor
-#
-# Copyright (C) 2024 Helge Magnus Keck <helgekeck@hotmail.com>
-#
-# This file may be distributed under the terms of the GNU GPLv3 license.
-from math import fabs
+# /usr/bin/python3
+
+import sys
+import re
+import argparse
 from shutil import ReadError, copy2
 from os import path, remove, getenv
-import os, logging, io
-import re
 
-#####
-# RatOS Gcode Post Processor
-#####
-class RatOS_Post_Processor:
+def argumentparser():
+	parser = argparse.ArgumentParser(
+		prog=path.basename(__file__),
+		description='** RatOS Gcode Postprocessor ** \n\r',
+		epilog='Result: Makes the GCode IDEX and RMMU ready.')
 
-	#####
-	# Initialize
-	#####
-	def __init__(self, config):
-		self.printer = config.get_printer()
-		self.name = config.get_name()
-		self.gcode = self.printer.lookup_object('gcode')
-		self.reactor = self.printer.get_reactor()
+	parser.add_argument('parameters', metavar='gcode-files', type=str, nargs='+',
+		help='One GCode file to be processed')
 
-		self.register_commands()
-		self.register_handler()
+	try:
+		args = parser.parse_args()
+		return args
+	except IOError as msg:
+		parser.error(str(msg))
 
-	def get_status(self, eventtime):
-		return {'name': self.name}
 
-	#####
-	# Handler
-	#####
-	def register_handler(self):
-		self.printer.register_event_handler("klippy:connect", self._connect)
+def main(args):
+	if "RMMU" in args.parameters:
+		sourcefile = args.parameters[1]
+	else:
+		sourcefile = args.parameters[0]
 
-	def _connect(self):
-		self.v_sd = self.printer.lookup_object('virtual_sdcard', None)
-		self.sdcard_dirname = self.v_sd.sdcard_dirname
-		self.dual_carriage = self.printer.lookup_object("dual_carriage", None)
-		self.rmmu_hub = self.printer.lookup_object("rmmu_hub", None)
+	if path.exists(sourcefile):
+		process_file(args, sourcefile, "RMMU" in args.parameters)
+	else:
+		print("File not found!\n")
 
-	#####
-	# Gcode commands
-	#####
-	def register_commands(self):
-		self.gcode.register_command('RATOS_POST_PROCESSOR', self.cmd_RATOS_POST_PROCESSOR, desc=(self.desc_RATOS_POST_PROCESSOR))
 
-	desc_RATOS_POST_PROCESSOR = ""
-	def cmd_RATOS_POST_PROCESSOR(self, gcmd):
-		if self.dual_carriage == None and self.rmmu_hub == None:
-			self.v_sd.cmd_SDCARD_PRINT_FILE(gcmd)
-		else:
-			filename = gcmd.get('FILENAME', "")
-			if filename[0] == '/':
-				filename = filename[1:]
-			if self.process_file(filename):
-				self.v_sd.cmd_SDCARD_PRINT_FILE(gcmd)
-			else:
-				raise self.printer.command_error("Could not process gcode file")
+def process_file(args, sourcefile, rmmu):
+	# read file into memory
+	try:
+		with open(sourcefile, "r", encoding='UTF-8') as readfile:
+			lines = readfile.readlines()
+	except ReadError as exc:
+		print('FileReadError: ' + str(exc))
+		sys.exit(1)
+	finally:
+		readfile.close()
 
-	#####
-	# Post Processor
-	#####
-	def process_file(self, filename):
-		path = self.get_file_path(filename)
-		lines = self.get_file_lines(path)
-
-		if self.already_processed(path):
-			return True
-
-		self.ratos_echo("processing...")
-
-		slicer = self.get_slicer(lines[0].rstrip())
-
-		if slicer["Name"] != "PrusaSlicer" and slicer["Name"] != "SuperSlicer":
-			raise self.printer.command_error("Unsupported Slicer")
-
+	slicer = ""
+	if "prusaslicer" in lines[0].rstrip().lower():
+		slicer = "prusaslicer"
+	elif "superslicer" in lines[0].rstrip().lower():
+		slicer = "superslicer"
+	
+	if slicer == "prusaslicer" or slicer == "superslicer":
+		# START_PRINT parameter and toolshift processing
 		min_x = 1000
 		max_x = 0
 		first_x = -1
@@ -87,32 +63,25 @@ class RatOS_Post_Processor:
 		file_has_changed = False
 		wipe_accel = 0
 		used_tools = []
-		pause_counter = 0
-		layer_number = 0
 		layer_number = 0
 		extruder_temps = []
 		extruder_temps_line = 0
 		for line in range(len(lines)):
-			# give the cpu some time
-			pause_counter += 1
-			if pause_counter == 1000:
-				pause_counter = 0
-				self.reactor.pause(.001)
 
 			# get slicer profile settings
-			if slicer["Name"] == "PrusaSlicer":
+			if slicer == "prusaslicer":
 				if wipe_accel == 0:
 					if lines[line].rstrip().startswith("; wipe_tower_acceleration = "):
 						wipe_accel = int(lines[line].rstrip().replace("; wipe_tower_acceleration = ", ""))
 
-			# get the start_print line number
+			# get the start_print line number and fix color variable format
 			if start_print_line == 0:
 				if lines[line].rstrip().startswith("START_PRINT") or lines[line].rstrip().startswith("RMMU_START_PRINT"):
-					lines[line] = lines[line].replace("#", "") # fix color variable format
 					start_print_line = line
+					lines[line] = lines[line].replace("#", "") # fix color variable format
 
 			# fix superslicer other layer temperature bug
-			if start_print_line > 0 and slicer["Name"] == "SuperSlicer":
+			if start_print_line > 0 and slicer == "superslicer":
 				if extruder_temps_line == 0:
 					if lines[line].rstrip().startswith(";LAYER_CHANGE"):
 						layer_number += 1
@@ -153,16 +122,18 @@ class RatOS_Post_Processor:
 								if x > first_x:
 									first_x = x
 							except Exception as exc:
-								self.ratos_echo("Can not get first x coordinate. " + str(exc))
-								return False
+								print("Cant get first x coordinate. " + str(exc) + "\n")
+								print("line:" + lines[line].rstrip())
+								sys.exit(1)
 						if split[s].lower().startswith("y"):
 							try:
 								y = float(split[s].lower().replace("y", ""))
 								if y > first_y:
 									first_y = y
 							except Exception as exc:
-								self.ratos_echo("Can not get first y coordinate. " + str(exc))
-								return False
+								print("Cant get first y coordinate. " + str(exc) + "\n")
+								print("line:" + lines[line].rstrip())
+								sys.exit(1)
 
 			# get x boundaries 
 			if start_print_line > 0:
@@ -177,12 +148,13 @@ class RatOS_Post_Processor:
 								if x > max_x:
 									max_x = x
 							except Exception as exc:
-								self.ratos_echo("Can not get x boundaries . " + str(exc))
-								return False
+								print("Oops! Something went wrong. " + str(exc) + "\n")
+								print("line:" + lines[line].rstrip())
+								sys.exit(1)
 
 			# toolshift processing
 			if start_print_line > 0:
-				if lines[line].rstrip().startswith("T") and lines[line].rstrip()[1:].isdigit():
+				if lines[line].rstrip().startswith("T0") or lines[line].rstrip().startswith("T1"):
 
 					# purge tower
 					if tower_line == -1:
@@ -208,7 +180,7 @@ class RatOS_Post_Processor:
 					# toolchange line
 					toolchange_line = 0
 					for i2 in range(20):
-						if lines[line + i2].rstrip().startswith("T") and lines[line].rstrip()[1:].isdigit():
+						if lines[line + i2].rstrip().startswith("T0") or lines[line + i2].rstrip().startswith("T1"):
 							toolchange_line = line + i2
 							break
 
@@ -257,25 +229,36 @@ class RatOS_Post_Processor:
 								break
 
 					# make toolshift changes
-					if toolshift_count > 0 and toolchange_line > 0 and move_line > 0:
+					if toolshift_count > 1 and toolchange_line > 0 and move_line > 0:
 						file_has_changed = True
+
 						if zhop_line > 0:
+							print("Z-Hop removed            " + lines[zhop_line].rstrip())
 							lines[zhop_line] = '; Removed by RatOS post processor: ' + lines[zhop_line].rstrip() + '\n'
+
 						if zdrop_line > 0:
+							print("Z-Drop removed           " + lines[zdrop_line].rstrip())
 							lines[zdrop_line] = '; Removed by RatOS post processor: ' + lines[zdrop_line].rstrip() + '\n'
-						if self.rmmu_hub == None:
+						if not rmmu:
 							new_toolchange_gcode = (lines[toolchange_line].rstrip() + ' ' + move_x + ' ' + move_y + ' ' + move_z).rstrip()
 						else:
 							new_toolchange_gcode = ('TOOL T=' + lines[toolchange_line].rstrip().replace("T", "") + ' ' + move_x.replace("X", "X=") + ' ' + move_y.replace("Y", "Y=") + ' ' + move_z.replace("Z", "Z=")).rstrip()
+						print('parameter added          ' + new_toolchange_gcode)
 						lines[toolchange_line] = new_toolchange_gcode + '\n'
+						print('Horizontal move removed  ' + lines[move_line].rstrip().replace("  ", " "))
 						lines[move_line] = '; Removed by RatOS post processor: ' + lines[move_line].rstrip().replace("  ", " ") + '\n'
+
 						if retraction_line > 0 and extrusion_line > 0:
+							print("Retraction move removed  " + lines[retraction_line].rstrip())
 							lines[retraction_line] = '; Removed by RatOS post processor: ' + lines[retraction_line].rstrip() + '\n'
+							print("Deretraction move removed   " + lines[extrusion_line].rstrip())
 							lines[extrusion_line] = '; Removed by RatOS post processor: ' + lines[extrusion_line].rstrip() + '\n'
+
+						print("\n")
 
 		# add START_PRINT parameters 
 		if start_print_line > 0:
-			if toolshift_count > 0:
+			if toolshift_count > 0 :
 				file_has_changed = True
 				lines[start_print_line] = lines[start_print_line].rstrip() + ' TOTAL_TOOLSHIFTS=' + str(toolshift_count - 1) + '\n'
 			if first_x >= 0 and first_y >= 0:
@@ -293,89 +276,19 @@ class RatOS_Post_Processor:
 					for tool in used_tools:
 						lines[extruder_temps_line] = lines[extruder_temps_line] + "M104 S" + str(extruder_temps[int(tool)]) + " T" + str(tool) + "\n"
 
-			# console output 
-			self.ratos_echo("USED TOOLS: " + ','.join(used_tools))
-			self.ratos_echo("TOOLSHIFTS: " + str(0 if toolshift_count == 0 else toolshift_count - 1))
-			self.ratos_echo("SLICER: " + slicer["Name"] + " " + slicer["Version"])
+		# save file if it has changed 
+		if file_has_changed:
+			writefile = None
+			try:
+				with open(sourcefile, "w", newline='\n', encoding='UTF-8') as writefile:
+					for i, strline in enumerate(lines):
+						writefile.write(strline)
+			except Exception as exc:
+				print("FileWriteError: " + str(exc) + "\n")
+				sys.exit(1)
+			finally:
+				writefile.close()
 
-			# save file if it has changed 
-			if file_has_changed:
-				lines[1] = lines[1].rstrip()+ "; processed by RatOS\n"
-				self.save_file(path, lines)
+ARGS = argumentparser()
 
-		self.ratos_echo("Done!")
-		return True
-
-	def already_processed(self, path):
-		readfile = None
-		try:
-			i = 0
-			with open(path, 'r', encoding='utf-8') as readfile:
-				for line in readfile:
-					i += 1
-					if i == 2:
-						return line.rstrip().lower().startswith("; processed by ratos")
-		except:
-			raise self.printer.command_error("Can not get processed state")
-		finally:
-			readfile.close()
-
-	def get_slicer(self, line):
-		try:
-			line = line.rstrip().replace("; generated by ", "")
-			split = line.split(" on ")[0]
-			return {"Name": split.split(" ")[0], "Version": split.split(" ")[1]}
-		except:
-			raise self.printer.command_error("Can not get slicer version")
-
-	def get_file_path(self, filename):
-		files = self.v_sd.get_file_list(True)
-		flist = [f[0] for f in files]
-		files_by_lower = { filepath.lower(): filepath for filepath, fsize in files }
-		filepath = filename
-		try:
-			if filepath not in flist:
-				filepath = files_by_lower[filepath.lower()]
-			return os.path.join(self.sdcard_dirname, filepath)
-		except:
-			raise self.printer.command_error("Can not get path for file " + filename)
-
-	def get_file_lines(self, filepath):
-		try:
-			with open(filepath, "r", encoding='UTF-8') as readfile:
-				return readfile.readlines()
-		except:
-			raise self.printer.command_error("Unable to open file")
-		finally:
-			readfile.close()
-
-	def save_file(self, path, lines):
-		writefile = None
-		try:
-			pause_counter = 0
-			with open(path, "w", newline='\n', encoding='UTF-8') as writefile:
-				for i, strline in enumerate(lines):
-					pause_counter += 1
-					if pause_counter == 1000:
-						pause_counter = 0
-						self.reactor.pause(.001)
-					writefile.write(strline)
-		except Exception as exc:
-			raise self.printer.command_error("FileWriteError: " + str(exc))
-		finally:
-			writefile.close()
-
-	#####
-	# Helper
-	#####
-	def ratos_echo(self, msg):
-		self.gcode.run_script_from_command("RATOS_ECHO PREFIX='POST_PROCESSOR' MSG='" + str(msg) + "'")
-
-	def ratos_debug_echo(self, msg):
-		self.gcode.run_script_from_command("DEBUG_ECHO PREFIX='POST_PROCESSOR' MSG='" + str(msg) + "'")
-
-#####
-# Loader
-#####
-def load_config(config):
-	return RatOS_Post_Processor(config)
+main(ARGS)
