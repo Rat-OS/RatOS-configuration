e6db1bde760ac83db15cac1bc9b04f8b67adbf67
ibrahimbaig4 <104411996+ibrahimbaig4@users.noreply.github.com>
2024-09-04 23:06:57 -0400

    Update make-and-flash-mcu.sh

--- boards/Mellow-super-8-pro-H723/make-and-flash-mcu.sh
+++ boards/Mellow-super-8-pro-H723/make-and-flash-mcu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # NOTE: This script ONLY WORKS if there's no SD card in the board AND the 
 # board has already been flashed with klipper via SD card.
-# NEXT SECTION IS ONLY RELEVANT IF USING THE boot0 JUMPER (NOT RECOMMENDED)
+# NEXT SECTION IS ONLY RELEVANT IF USING THE bt0/3.3v JUMPER (NOT RECOMMENDED)
 # The first time the board is booted after the jumper has been installed
 # the board needs to be flashed via the dfu vendor:device id. After that
-# it can be flashed via the /dev/btt-octopus-pro-446 path, but it then fails
+# it can be flashed via the /dev/mellow-super-8-pro path, but it then fails
 # to exit dfu mode, and then one needs to flash it yet again with the
 # vendor:device id. Then it correctly exits DFU mode. Except those times
 # where it doesn't, for that we have a 3rd pass...
