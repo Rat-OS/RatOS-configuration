efa051bda99be282343384f423a69d298a1dc768
Travis <basslingo@gmail.com>
2024-09-25 20:29:50 -0500

    Scripts remove legacy hostname scripts (#296)
    * remove legacy rename scripts
    
    Signed-off-by: Travis Baker <funtimeerror@gmail.com>
    
    * remove whitelisting of command that no longer exists
    
    Signed-off-by: Travis Baker <funtimeerror@gmail.com>
    
    ---------
    
    Signed-off-by: Travis Baker <funtimeerror@gmail.com>
    Co-authored-by: Travis Baker <funtimeerror@gmail.com>

--- scripts/ratos-common.sh
+++ scripts/ratos-common.sh
@@ -253,18 +253,5 @@ pi  ALL=(ALL) NOPASSWD: /home/pi/printer_data/config/RatOS/scripts/moonraker-upd
 	$sudo chown root:root /tmp/030-ratos-githooks
 	$sudo chmod 440 /tmp/030-ratos-githooks
 	$sudo cp --preserve=mode /tmp/030-ratos-githooks /etc/sudoers.d/030-ratos-githooks
-
-	# Whitelist change hostname script
-	if [[ ! -e /etc/sudoers.d/031-ratos-change-hostname ]]
-	then
-		touch /tmp/031-ratos-change-hostname
-		cat << '#EOF' > /tmp/031-ratos-change-hostname
-pi  ALL=(ALL) NOPASSWD: /home/pi/printer_data/config/RatOS/scripts/change-hostname-as-root.sh
-#EOF
-
-		$sudo chown root:root /tmp/031-ratos-change-hostname
-		$sudo chmod 440 /tmp/031-ratos-change-hostname
-		$sudo cp --preserve=mode /tmp/031-ratos-change-hostname /etc/sudoers.d/031-ratos-change-hostname
-	fi
 }
 
